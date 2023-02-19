2388d0748d89f67586ba5562a0772e4e5cc62fef
bxchaney <bxchaney@gmail.com>
2023-02-19 17:54:13 -0500

    experimenting with file io

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include<iostream>
+#include<stdio.h>
 #include "OperatorStack.cpp"
 
 int main(int argc, char** argv)
 {
-    std::cout << argc << std::endl;
-    std::cout << argv[1] << std::endl;
+    if (argc == 1) return 0;
+    
+
+
+    FILE* file;
+    file = fopen(argv[1], "r");
+
+    int c;
+    if (file)
+    {
+        while((c = fgetc(file)) != EOF)
+        {
+            putchar(c);
+        }
+    }
+
+    fclose(file);
+
 
 }
