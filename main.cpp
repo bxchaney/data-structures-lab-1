988de75cfcb824bce391b725f192016492bb46cf
bxchaney <bxchaney@gmail.com>
2023-02-25 10:21:14 -0500

    now reading input

--- main.cpp
+++ main.cpp
@@ -1,26 +1,66 @@
 #include<iostream>
 #include<stdio.h>
-#include "OperatorStack.cpp"
+#include"PreToPostConverter.cpp"
+#include"CharStack.cpp"
 
 int main(int argc, char** argv)
 {
-    if (argc == 1) return 0;
-    
-
-
-    FILE* file;
-    file = fopen(argv[1], "r");
-
-    int c;
-    if (file)
+    if (argc < 3) 
     {
-        while((c = fgetc(file)) != EOF)
-        {
+        std::cout << "Expecting at least 2 arguments, the first being the"
+                     " input file and the second being the output file." 
+                     << std::endl;
+        return -1;
+    }
+    FILE* input;
+    // FILE* output;
+    
+    input = fopen(argv[1], "r");
+    if (!input)
+    {
+        std::cout << "Could not open the input file, "
+                     "please check its name" << std::endl;
+        fclose(input);
+        return -1;
+    }
+    // output = fopen(argv[2], "w");
+
+    // if (!output)
+    // {
+    //     std::cout << "Could not open the output file." << std::endl;
+    //     fclose(output);
+    //     return -1;
+    // }
+    
+    int c;
+    CharStack echoed_input = CharStack();
+    PreToPostConverter postfix = PreToPostConverter();
+    while((c = fgetc(input)) != EOF)
+    {
+        
+        if (c == 13 || c == 9 || c == 32) {
+            continue;
+        }
+        if (c == 10) { //|| c == 13) {
+            printf(
+                "%s -> %s",
+                echoed_input.get_str(),
+                postfix.get_output()
+            );
+            echoed_input.reset();
+            postfix.reset();
+            putchar(13);
             putchar(c);
+        } else {
+            echoed_input.pushc(c);
+            postfix.next_character(c);
         }
     }
+    
 
-    fclose(file);
+    fclose(input);
+
+        
 
 
 }
