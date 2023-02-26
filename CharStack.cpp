447709d71b3afebfdbf0bf5540e19a2318ea7b3b
bxchaney <bxchaney@gmail.com>
2023-02-26 16:15:31 -0500

    added comments

--- CharStack.cpp
+++ CharStack.cpp
@@ -3,22 +3,40 @@
 
 CharStack::CharStack()
 {
+    
+    // defined in CharStack.hpp, DEFAULT_STR_SIZE = 10
     _max_size = DEFAULT_STR_SIZE;
+    
+    // char array allocated on the stack
     _string = new char[_max_size];
     _size = 0;
 }
 
-/// @brief 
+CharStack::~CharStack()
+{
+    // delete the array allocated to the heap.
+    delete _string;
+}
+
+/// @brief doubles max size, allocates new array with new max size, deletes
+/// old character array
 void CharStack::resize()
 {
     char* old_arr = _string;
+    
     _max_size *= 2;
+    
+    
     _string = new char[_max_size];
+    
+    // copy old array into new array
     for (int i=0; i<_size; i++)
     {
         _string[i] = old_arr[i];
     }
 
+    // the old array was allocated on the heap and is no longer needed
+    // so we delete
     delete old_arr;
 }
 
@@ -27,10 +45,15 @@ void CharStack::resize()
 void CharStack::pushc(char c)
 {
     if (_size == _max_size) resize();
+    
+    
     _string[_size] = c;
     _size++;
 }
 
+/// @brief pops the top char from the stack
+/// @return if CharStack is empty, returns '\0', otherwise returns the top
+/// char on the stack
 char CharStack::popc()
 {
     if (is_empty())
@@ -42,6 +65,8 @@ char CharStack::popc()
     return c;
 }
 
+/// @brief checks if the stack is empty
+/// @return true when the stack is empty
 bool CharStack::is_empty()
 {
     return _size == 0;
@@ -62,11 +87,14 @@ const char* CharStack::get_str()
     return string;
 }
 
+/// @brief resets the size of the stack to 0, old information is overwritten
 void CharStack::reset()
 {
     _size = 0;
 }
 
+/// @brief the current size of CharStack
+/// @return the integer size of CharStack
 int CharStack::size()
 {
     return _size;
