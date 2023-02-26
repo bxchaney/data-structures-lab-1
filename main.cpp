5a1c9a52bbf94696c46aec2f7154421d3a9f5f10
bxchaney <bxchaney@gmail.com>
2023-02-26 18:49:49 -0500

    commented main

--- main.cpp
+++ main.cpp
@@ -35,6 +35,7 @@ bool is_equal_str(char* s, const char* str)
 
 int main(int argc, char** argv)
 {
+    // Confirm there is an input and an output file
     if (argc < 3) 
     {
         std::cout << "Expecting at least 2 arguments, the first being the"
@@ -46,6 +47,7 @@ int main(int argc, char** argv)
     FILE* output;
     
     input = fopen(argv[1], "r");
+    // Checking input file
     if (!input)
     {
         std::cout << "Could not open the input file, "
@@ -54,7 +56,7 @@ int main(int argc, char** argv)
         return -1;
     }
     output = fopen(argv[2], "w");
-
+    // checking output file
     if (!output)
     {
         std::cout << "Could not open the output file." << std::endl;
@@ -64,14 +66,16 @@ int main(int argc, char** argv)
     
     int c;
     const char* str;
-    FixConverter converter = FixConverter();
+    const char* input_str;
+    FixConverter converter {};
+    fputs("Input -> Output", output);
     // -r flag indicates that the user is providing postfix expressions
     // to be converted to prefix.
     if (argc >= 4 && is_equal_str(argv[3],"-r"))
     {
         CharStack stack = CharStack();
         bool converter_empty;
-        const char* input_str;
+        
         printf(
             "Converting postfix expressions in %s to prefix and saving"
             " results in %s\n", argv[1], argv[2]
@@ -86,32 +90,43 @@ int main(int argc, char** argv)
             // if we reach a newline character, process and write chars
             if (c == 10)
             {
+                // A reversed valid postfix expression is a valid prefix
+                // expression. We can parse postfix expressions using the same
+                // objects by reversing their inputs to make them postfix
+                // expressions and then reversing the resulting postfix
+                // to get the corresponding correct prefix expression.
+
                 input_str = stack.get_str();
                 while (!stack.is_empty()) 
                 {
+                    // reverse the postfix input and parse
                     converter.next_character(stack.popc());
                 }
-                converter_empty = converter.output_length() == 0;
+                converter_empty = converter.input_length() == 0;
+                
+                // check for illegal characters
                 if (converter.has_illegal_characters())
                 {
                     str = "This expression has illegal characters";
                 }
+                // confirm the expression was valid
                 else if (!converter.is_valid())
                 {
                     str = "This is not a valid postfix expression";
                 }
                 else 
                 {
-                    // this output needs to be reversed
-                    
+                    // reverse the output to get prefix expression!
                     str = converter.get_output_reversed();
                 }
 
+                // print to terminal
                 printf(
                     "Input: %-20s Output: %-20s",
                     input_str,
                     str
                 );
+                // write to output file
                 if (!converter_empty)
                 {
                     fputs(input_str, output);
@@ -125,6 +140,7 @@ int main(int argc, char** argv)
                 putchar(13);
                 putchar(c);
             }
+            // otherwise, store next character in stack
             else
             {
                 stack.pushc(c);
@@ -137,6 +153,7 @@ int main(int argc, char** argv)
         printf(
             "Converting prefix expressions in %s to postfix and saving"
             " results in %s\n", argv[1], argv[2]);
+        
         while((c = fgetc(input)) != EOF)
         {
             // Ignoring register clears, spaces, and tabs
@@ -145,30 +162,34 @@ int main(int argc, char** argv)
             }
             // Once we read a newline character, process and write results
             if (c == 10) { 
+                input_str = converter.get_input();
                 
+                // check if illegal characters
                 if (converter.has_illegal_characters())
                 {
                     str = "This expression contains illegal characters";
                 }
+                // confirm the input was a valid expression
                 else if (!converter.is_valid())
                 {
                     str = "This is not a valid prefix expression";
                 }
+                // get the output
                 else 
                 {
                     str = converter.get_output();
                 }
 
-                
+                // write to terminal
                 printf(
                     "Input: %-20s Output: %-20s",
-                    converter.get_input(),
+                    input_str,
                     str
                 );
-
-                if (converter.output_length() > 0)
+                // write to output file
+                if (converter.input_length() > 0)
                 {
-                    fputs(converter.get_input(), output);
+                    fputs(input_str, output);
                     fputs(" -> ", output);
                     fputs(str, output);
                 }
@@ -177,11 +198,15 @@ int main(int argc, char** argv)
                 converter.reset();
                 putchar(13);
                 putchar(c);
-            } else {
+            } 
+            // process next character
+            else 
+            {
                 converter.next_character(c);
             }
         }
     }
+
     fclose(input);
     fclose(output);
 }
